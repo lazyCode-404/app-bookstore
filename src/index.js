c200f929fde4834293e6f9e0cd2c1569d1a4802e
SiansandaTamara <tamarasiansanda18@gmail.com>
2022-06-11 16:45:33 +0200

    fixed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import './index.css';
+import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -12,3 +11,7 @@ root.render(
     </Router>
   </React.StrictMode>,
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
