02aa31c3ffd691ac6040b038128a0d2da3461e54
SiansandaTamara <tamarasiansanda18@gmail.com>
2022-06-14 11:56:16 +0200

    add categories files

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -4,9 +4,11 @@ import {
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import booksReducer from './books/books';
+import categoryReducer from './categories/categories';
 
 const reducer = combineReducers({
   booksReducer,
+  categoryReducer,
 });
 
 const store = createStore(reducer, compose(applyMiddleware(thunk, logger)));
