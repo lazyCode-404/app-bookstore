b583f01ffabb0085baca8ea740e668703fa51fe6
SiansandaTamara <tamarasiansanda18@gmail.com>
2022-06-14 12:01:12 +0200

    add categories files

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,11 +1,8 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
-// import Books from './Books';
 
 const BookContent = () => (
   <ul className="book-content">
     {
-// books.map((book) => <Books key={book.id} book={book} />)
 }
   </ul>
 
