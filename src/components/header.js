0582d0bdc3627d13be818ab5518f5d5c1df72225
SiansandaTamara <tamarasiansanda18@gmail.com>
2022-06-21 13:56:21 +0200

    add header ,add bookmlist

--- src/components/header.js
+++ src/components/header.js
@@ -1,28 +1,24 @@
-import React from 'react';
-import BookContent from './Book';
-import AddBook from './AddBook';
-
-const BookContainer = () => {
-  const books = [
-    {
-      title: 'how to become a milionaire',
-      author: 'shepherd bushiri',
-      id: 1,
-      category: 'Novel',
-    },
-    {
-      title: 'parach daily',
-      author: 'shepherd bushiri',
-      id: 2,
-      category: 'Novel',
-    },
-  ];
-
-  return (
-    <div className="main-container">
-      <BookContent books={books} />
-      <AddBook />
+import { Link } from 'react-router-dom';
+import { ImMan } from 'react-icons/im';
+/* eslint-disable react/jsx-key */
+const Header = () => (
+  <div className="nav-card d-flex">
+    <div className="nav-content d-flex">
+      <h3>Bookstore CMS</h3>
+      <ul className="navbar d-flex">
+        <li className="menu-item">
+          <Link className="active-nav" to="/">Home</Link>
+        </li>
+        <li className="menu-item">
+          <Link className="non-active-nav" to="/categories">Categories</Link>
+        </li>
+      </ul>
+      <div className="icon-div">
+        <ImMan className="icon" />
+      </div>
     </div>
-  );
-};
-export default BookContainer;
+  </div>
+
+);
+
+export default Header;
