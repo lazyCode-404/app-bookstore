73ec5ab7f13d9bba82a383d0ba7270c5fa53d08a
Tamara Siansanda <tamarasiansanda18@gmail.com>
2022-06-23 15:40:39 +0200

    Update books.js

--- src/Ui/books.js
+++ src/Ui/books.js
@@ -1,5 +1,5 @@
-import bookForm from '../components/bookForm';
-import bookList from '../components/bookList';
+import BookForm from '..components/bookForm';
+import BookList from '..components/bookList';
 import Header from '../components/header';
 /* eslint-disable react/jsx-key */
 const Books = () => (
